fad89f2786072b530d2156a2a006b443a9ec592c
Jacob Allen <jedijake@me.com>
2017-01-28 13:40:27 -0800

    ng g c another-component --is --it

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <h1>Inline HTML</h1>
+      <fa-other></fa-other>
+      <fa-another></fa-another>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- first-app/src/app/other/other.component.ts
+++ first-app/src/app/other/other.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-other',
+  selector: 'fa-other',
   templateUrl: './other.component.html',
   styleUrls: ['./other.component.css']
 })
